85e6837d7819ab49c96a932151de0f5103b83ef1
AlanCienega <craulalan115@gmail.com>
2022-11-03 16:45:34 -0600

    pasan los test
    pasan los test al 100 y se documentan las funciones

--- src/VillaPeruana.php
+++ src/VillaPeruana.php
@@ -5,11 +5,23 @@ namespace App;
 class VillaPeruana
 {
     public $name;
-
     public $quality;
-
     public $sellIn;
 
+    public const NORMAL = "normal";
+    public const PISCO = "Pisco Peruano";
+    public const TUMI =  "Tumi de Oro Moche";
+    public const VIP = "Ticket VIP al concierto de Pick Floid";
+    public const ALTOCUSCO = "Café Altocusco";
+
+    /**
+     * lo de siempre, se encarga de que pasemos estos datos a fuerza
+     *
+     * @param  mixed $name
+     * @param  mixed $quality
+     * @param  mixed $sellIn
+     * @return void
+     */
     public function __construct($name, $quality, $sellIn)
     {
         $this->name = $name;
@@ -17,57 +29,135 @@ class VillaPeruana
         $this->sellIn = $sellIn;
     }
 
-    public static function of($name, $quality, $sellIn) {
+    /**
+     * clase of
+     * para pasarle los argumentos y poderlos usar libremente
+     *
+     * @param  mixed $name
+     * @param  mixed $quality
+     * @param  mixed $sellIn
+     * @return void
+     */
+    public static function of($name, $quality, $sellIn)
+    {
         return new static($name, $quality, $sellIn);
     }
 
+    /**
+     * esta funcion es la encargada de simular el paso de un dia en Vila Peruana
+     * 
+     * Al final de cada día, los valores se disminuyen en ambos valores.
+     * Cuando la fecha de venta ya paso, es decir es menor a -1, los productos se degradan
+     * dos veces mas rapido, es decir en 2.
+     * Nos debemos asegurar que el quality nunca sea negativo. (lo dejare en 0)
+     * Los productos "Pisco Peruano", en realidad, incrementan en Quality mientras más viejos están
+     * El Quality de un producto nunca es mayor a 50 (aunque se trate del pisco peruano)
+     * Los productos "Tumi", siendo un producto legendario, nunca debe ser vendido o bajaría su Quality.
+     * Los "Tickets VIP", así como "Pisco Peruano", incrementan su Quality conforme su SellIn se acerca a 0,
+     * el Quality incrementa en 2 cuando faltan 10 días o menos y en 3 cuando faltan 5 días o menos,
+     * pero el Quality disminuye a 0 después del concierto.
+     * 
+     * Los productos de "Café" se degradan en Quality el doble que los productos normales, eso quiere decir que
+     * vamos a degradarlo en 2 por cada dia que pase si es antes de su fecha de venta, y e 4 despues
+     * ya que el producto normal se degrada en 2.
+     * 
+     *  un producto nunca puede incrementar su Quality mayor a 50, sin embargo "Tumi" es un producto legendario 
+     * y como tal su Quality es 80 y nunca cambia.
+     *
+     * @return void
+     */
     public function tick()
     {
-        if ($this->name != 'Pisco Peruano' and $this->name != 'Ticket VIP al concierto de Pick Floid') {
-            if ($this->quality > 0) {
-                if ($this->name != 'Tumi de Oro Moche') {
-                    $this->quality = $this->quality - 1;
-                }
-            }
-        } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
-
-                if ($this->name == 'Ticket VIP al concierto de Pick Floid') {
-                    if ($this->sellIn < 11) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                    if ($this->sellIn < 6) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if ($this->name != 'Tumi de Oro Moche') {
-            $this->sellIn = $this->sellIn - 1;
-        }
-
-        if ($this->sellIn < 0) {
-            if ($this->name != 'Pisco Peruano') {
-                if ($this->name != 'Ticket VIP al concierto de Pick Floid') {
-                    if ($this->quality > 0) {
-                        if ($this->name != 'Tumi de Oro Moche') {
-                            $this->quality = $this->quality - 1;
-                        }
-                    }
+        switch ($this->name) {
+            case VillaPeruana::NORMAL;
+                if ($this->sellIn <= 0) {
+                    $this->doubleDegradation();
                 } else {
-                    $this->quality = $this->quality - $this->quality;
+                    $this->simpleDegradation();
                 }
-            } else {
-                if ($this->quality < 50) {
-                    $this->quality = $this->quality + 1;
+                $this->setQuality();
+                $this->sellIn -= 1;
+                break;
+            case VillaPeruana::PISCO;
+                $this->producPiscotDemand();
+                $this->setQuality();
+                $this->sellIn -= 1;
+                break;
+            case VillaPeruana::TUMI;
+                break;
+            case VillaPeruana::VIP;
+                $this->productDemand();
+                $this->setQuality();
+                $this->sellIn -= 1;
+                break;
+            case VillaPeruana::ALTOCUSCO;
+                if ($this->sellIn <= 0) {
+                    $this->quality -= 4;
+                } else {
+                    $this->doubleDegradation();
                 }
-            }
+                $this->setQuality();
+                $this->sellIn--;
+                break;
+        }
+    }
+    function simpleDegradation()
+    {
+        $this->quality--;
+    }
+    public function doubleDegradation()
+    {
+        $this->quality -= 2;
+    }
+
+    /**
+     * definimos la calidad del producto vip que vale mas cada que pasan los dias antes de su fecha de venta
+     * despues de eso el producto ya no vale nada, es decir su calidad baja a 0
+     *
+     * @return void
+     */
+    public function productDemand()
+    {
+        if ($this->sellIn <= 0) {
+            $this->quality = 0;
+        } else if ($this->sellIn <= 5) {
+            $this->quality += 3;
+        } else if ($this->sellIn <= 10) {
+            $this->quality += 2;
+        } else {
+            $this->quality++;
+        }
+    }
+    /**
+     * se encarga de definir la calidad del producto llamado pisco
+     *
+     * @return void
+     */
+    public function producPiscotDemand()
+    {
+        if (($this->sellIn < 0) && ($this->name == VillaPeruana::VIP)) {
+            $this->quality = 0;
+        } else if ($this->sellIn < 5) {
+            $this->quality += 2;
+        } else {
+            $this->quality++;
+        }
+    }
+
+    /**
+     * esta funcion se encarga de definir la calidad del producto despues de haberse hecho todas las operaciones
+     * para cada caso, para la mayoria caen en el mismo valor, pero para productos pisco y vip, su maxima calidad
+     * sera de 50.
+     *
+     * @return void
+     */
+    function setQuality()
+    {
+        if (($this->name == VillaPeruana::PISCO || $this->name == VillaPeruana::VIP) && $this->quality > 50) {
+            $this->quality = 50;
+        }
+        if ($this->quality < 0) {
+            $this->quality = 0;
         }
     }
 }
